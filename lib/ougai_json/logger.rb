80a4cc56f129e126e2ec70448bb23f95e3a9c67c
Jack.Wu <jwu4000@outlook.com>
2024-02-28 18:26:32 +0800

    rename

--- lib/ougai_json/logger.rb
+++ lib/ougai_json/logger.rb
@@ -4,18 +4,20 @@ require 'ougai_json/logger/formatter'
 require "ougai_json/logger/engine"
 
 module OugaiJson
-  class Logger < Ougai::Logger
-    def initialize(logdev, **)
-      super
-      @logdev = MonoLogger::LocklessLogDevice.new(logdev)
-    end
+  module Logging
+    class Logger < Ougai::Logger
+      def initialize(logdev, **)
+        super
+        @logdev = MonoLogger::LocklessLogDevice.new(logdev)
+      end
 
-    def create_formatter
-      return OugaiJson::Logger::Formatter.new
-    end
+      def create_formatter
+        return OugaiJson::Logger::Formatter.new
+      end
 
-    def self.custom_logger(name: '', level: Rails.logger.present? ? Rails.logger.level : MonoLogger::INFO, type: :ougai)
-      new($stdout, level: level, progname: name)
+      def self.custom_logger(name: '', level: Rails.logger.present? ? Rails.logger.level : MonoLogger::INFO, type: :ougai)
+        new($stdout, level: level, progname: name)
+      end
     end
   end
 end
